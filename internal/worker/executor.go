7a0636331950ffea76e4111abefec0e9c19eab68
cloudcarver <mikechesterwang@foxmail.com>
2025-03-27 17:06:35 +0800

    run diagnostic

--- internal/worker/executor.go
+++ internal/worker/executor.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/risingwavelabs/wavekit/internal/apigen"
+	"github.com/risingwavelabs/wavekit/internal/conn/http"
 	"github.com/risingwavelabs/wavekit/internal/conn/meta"
 	"github.com/risingwavelabs/wavekit/internal/model"
 	"github.com/risingwavelabs/wavekit/internal/model/querier"
@@ -104,5 +105,48 @@ func (e *Executor) ExecuteAutoBackup(ctx context.Context, spec apigen.TaskSpecAu
 }
 
 func (e *Executor) ExecuteAutoDiagnostic(ctx context.Context, spec apigen.TaskSpecAutoDiagnostic) error {
+	cluster, err := e.model.GetClusterByID(ctx, spec.ClusterID)
+	if err != nil {
+		return errors.Wrap(err, "failed to get cluster")
+	}
+	tz, err := e.model.GetTimeZone(ctx, cluster.OrganizationID)
+	if err != nil {
+		return errors.Wrap(err, "failed to get timezone")
+	}
+	config, err := e.model.GetAutoDiagnosticsConfig(ctx, spec.ClusterID)
+	if err != nil {
+		return errors.Wrap(err, "failed to get auto diagnostics config")
+	}
+
+	// schedule the next task
+	parser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
+	cron, err := parser.Parse(fmt.Sprintf("TZ=%s %s", tz, config.CronExpression))
+	if err != nil {
+		return errors.Wrapf(err, "failed to parse cron expression: %s", config.CronExpression)
+	}
+	next := cron.Next(time.Now())
+	if _, err := e.model.CreateTask(ctx, querier.CreateTaskParams{
+		Spec: apigen.TaskSpec{
+			Type:           apigen.AutoDiagnostic,
+			AutoDiagnostic: &spec,
+		},
+		Status:    string(apigen.Pending),
+		StartedAt: &next,
+	}); err != nil {
+		return errors.Wrap(err, "failed to create task")
+	}
+
+	// run diagnostics
+	conn := http.NewMetaHttpConnection(fmt.Sprintf("http://%s:%d", cluster.Host, cluster.HttpPort))
+	content, err := conn.GetDiagnose(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get diagnose")
+	}
+	if _, err := e.model.CreateClusterDiagnostic(ctx, querier.CreateClusterDiagnosticParams{
+		ClusterID: cluster.ID,
+		Content:   content,
+	}); err != nil {
+		return errors.Wrap(err, "failed to create cluster diagnostic")
+	}
 	return nil
 }
